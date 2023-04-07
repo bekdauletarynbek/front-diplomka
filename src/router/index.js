a326d6d8f6befe830a11ab0c2d0862663371c5d5
bekdaulet <bekdauletarynbek@gmail.com>
2023-04-07 17:38:53 +0600

    deploying

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -18,8 +18,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/front/'),
-  base: '/front/',
+  history: createWebHashHistory(''),
   routes
 })
 
